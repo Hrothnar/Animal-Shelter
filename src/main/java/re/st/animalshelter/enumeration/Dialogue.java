d4033d03ad3fda143a6d5083fc43a8c86f8870f1
Kirill <hrothnar@mail.ru>
2023-04-20 19:42:09 +0600

    Added the ability to send photo, created a Photo class, changed the file reading system

--- src/main/java/re/st/animalshelter/enumeration/Dialogue.java
+++ src/main/java/re/st/animalshelter/enumeration/Dialogue.java
@@ -2,15 +2,15 @@ package re.st.animalshelter.enumeration;
 
 import re.st.animalshelter.enumeration.animal.Shelter;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Objects;
 
 public enum Dialogue {
     START,
     MENU,
-    INFO;
+    INFO,
+    MAP;
 
 //    DRIVER_PERMIT,
 //    COMMUNICATION_DATA,
@@ -23,45 +23,44 @@ public enum Dialogue {
 //    PHOTO_WAITING,
 //    TEXT_WAITING,
 
-    private String text; // названия файлов должны соответствовать названиям Enums, с учётом префиксов типа приюта и статуса держателя
-    private static final String OWNER_PREFIX = "owner_"; // префикс для метки файлов, текст которых, предназначен для держателей животных
-    private static final String USER_PREFIX = "user_"; // префикс для метки файлов, текст которых, предназначен для простых пользователей
-    private static final String PATH = "./src/main/resources/text/"; // просто путь...без цели...ъуъ...
+    private String text = ""; // названия файлов должны соответствовать названиям Enums, с учётом префиксов типа приюта и статуса держателя
+    private File file;
+    private static final String TEXT = "text/";
+    private static final String PHOTO = "photo/";
+    private static final String RESOURCES = "src/main/resources/";
 
-    public String getText(Shelter shelter, boolean isOwner) {
-        readFromFile(shelter, isOwner);
+    public String getText(Shelter shelter, boolean isOwner, Extension extension) {
+        getData(TEXT, shelter, isOwner, extension.getExtension());
         return text;
     }
 
-    private void readFromFile(Shelter shelter, boolean isOwner) {
-        String fileName = this.toString().toLowerCase();
-        try {
-            if (Objects.equals(shelter, Shelter.CAT)) {
-                if (isOwner) {
-                    text = Files.readString(Path.of(PATH, "owner/cat", OWNER_PREFIX + Shelter.CAT.getPrefix() + fileName + ".md"));
-                } else {
-                    text = Files.readString(Path.of(PATH, "user/cat", USER_PREFIX + Shelter.CAT.getPrefix() + fileName + ".md"));
-                }
-            } else if (Objects.equals(shelter, Shelter.DOG)) {
-                if (isOwner) {
-                    text = Files.readString(Path.of(PATH, "owner/dog", OWNER_PREFIX + Shelter.DOG.getPrefix() + fileName + ".md"));
-                } else {
-                    text = Files.readString(Path.of(PATH, "user/dog", USER_PREFIX + Shelter.DOG.getPrefix() + fileName + ".md"));
-                }
-            } else if (Objects.equals(shelter, Shelter.NONE)) {
-                if (isOwner) {
-                    text = Files.readString(Path.of(PATH, "owner", OWNER_PREFIX + Shelter.NONE.getPrefix() + fileName + ".md"));
-                } else {
-                    text = Files.readString(Path.of(PATH, "user", USER_PREFIX + Shelter.NONE.getPrefix() + fileName + ".md"));
-                }
-            } else {
-                //TODO ошибка
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-            //TODO своё исключение
-        }
+    public File getPhoto(Shelter shelter, boolean isOwner, Extension extension) {
+        getData(PHOTO, shelter, isOwner, extension.getExtension());
+        return file;
     }
 
-
+    private void getData(String mediaType, Shelter shelter, boolean isOwner, String extension) {
+        String fileName = this.toString().toLowerCase();
+        String shelterPath = shelter.getPath();
+        String shelterPrefix = shelter.getPrefix();
+        String personPath = "user/";
+        String personPrefix = "user_";
+        if (isOwner) {
+            personPrefix = "owner_";
+            personPath = "owner/";
+        }
+        try {
+            switch (mediaType) {
+                case TEXT:
+                    File toFile = new File("./" + RESOURCES + mediaType + personPath + shelterPath + personPrefix + shelterPrefix + fileName + extension);
+                    if (toFile.exists()) text = Files.readString(toFile.toPath());
+                    break;
+                case PHOTO:
+                    file = new File(RESOURCES + mediaType + personPath + shelterPath + personPrefix + shelterPrefix + fileName + extension);
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
