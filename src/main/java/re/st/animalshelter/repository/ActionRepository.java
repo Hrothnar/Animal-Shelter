b33acab85f500820074786dfc7d43d3ee0701a18
Kirill <hrothnar@mail.ru>
2023-05-19 20:39:01 +0600

    Fixed changes

--- src/main/java/re/st/animalshelter/repository/ActionRepository.java
+++ src/main/java/re/st/animalshelter/repository/ActionRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import re.st.animalshelter.entity.Action;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,7 +13,5 @@ public interface ActionRepository extends JpaRepository<Action, Long> {
 
     @Query(value = "SELECT a FROM actions a WHERE a.messageId = :messageId AND a.id = (SELECT MAX(id) FROM actions WHERE messageId = :messageId)")
     Optional<Action> findLastActionByMessageId(@Param("messageId") int messageId);
-//
-//    @Query(value = "SELECT a FROM actions a WHERE a.messageId = :messageId AND a.id IN (SELECT TOP (2, a2.id) FROM actions a2 WHERE a2.messageId = :messageId ORDER BY a2.id DESC)")
-//    LinkedList<Action> findTwoLastActionsByMessageId(@Param("messageId") int messageId);
+
 }
